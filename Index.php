e3a6619183b62e76bb45d520de0ae43e928fee35
farah <abfrad@gmail.com>
2014-04-15 22:54:24 +0200

    Layout Phase 001: 1% of Layout implemented

---
+++ Index.php
@@ -0,0 +1,33 @@
+<?php
+echo "
+<html>
+	<head>
+		<link rel='stylesheet' type='text/css' href='style/look.css' > 
+	
+	</head>
+";
+echo "
+	<body>
+		<div id='container'>
+		
+			<div id='chatroom'>
+			
+				<div id='messagebox'>
+				</div>
+			
+				<div id='writingbox'>
+					<form>
+						<input type='text'  id='chatty' >
+						<input type='button' value='Send'>
+					</form>
+				</div>
+				
+			</div>
+			
+		</div>
+	</body>
+";
+echo "
+</html>";
+
+?>
